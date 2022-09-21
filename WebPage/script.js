b1847c6a4dbd71daea0791db17dd6bab413c900d
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2022-09-21 14:15:11 -0600

    created basic functioning webpage for BOCC and added analyses to ipython notebooks

--- WebPage/script.js
+++ WebPage/script.js
@@ -14,15 +14,81 @@ fetch(url)
     });
   });
 
+function clean_and_split_input(text){
+    var text_list = text.split(/\n/);
+    // remove white spaces from HPO terms
+    var clean_list = [];
+    for (var i = 0; i < text_list.length; i++){
+        item = text_list[i];
+        if (typeof item == 'undefined'){
+            continue;
+        }
+        item = item.replace(/\s/g, "");
+        clean_list.push(item);
+    }
+    return clean_list;
+}
+
+function combine_and_bolden_cluster(gene,hpo,cluster){
+    /*
+    combine the cluster list into a string and place bold tags around the gene and HPO that matched in the cluster
+    */
+    var cluster_text = ""
+    for(var i = 0; i < cluster.length; i++){
+        if(i == 0){
+            cluster_text += '<b>' + cluster[i] + '</b>';
+            continue;
+        }
+        if( cluster[i] == gene || cluster[i] == hpo){
+            cluster_text += ', <b>' + cluster[i] + '</b>';
+        }else{
+            cluster_text += ', ' + cluster[i];
+        }
+    }
+    return cluster_text;
+}
+
+function search_clusters(hpos, genes, clusters){
+    /*
+    Input:
+    Take a list of genes, hpos and clusters, display clusters containing gene & hpo co-occurances
+    genes: list of genes. ['string1','string2'...]
+    hpos: list of hpos. ['string1','string2'...]
+    clusters: list clusters. [ ['gene1','hpo1','hpo2'...], ['gene2','gene3','hpo3'...], ... ]
+    */
+    var res_text = "";
+    for (var i = 0; i < genes.length; i++) {
+        gene = genes[i]
+        for (var j = 0; j < hpos.length; j++) {
+            hpo = hpos[j]
+            for (var k = 0; k < clusters.length; k++) {
+                cluster = clusters[k]
+                if(cluster.includes(hpo, 1) & cluster.includes(gene, 1)){
+                    console.log('Found')
+                    console.log(hpo)
+                    console.log(gene)
+                    console.log(cluster)
+                    res_text += '<div>' + combine_and_bolden_cluster(gene,hpo,cluster) + '</div></br>'
+                }
+            }
+        }
+    }
+    document.getElementById("resultslist").innerHTML = res_text;
+}
+
 window.onload = function(){
     document.getElementById('search_button').onclick = function() {
         console.log('Clicked button!')
         // get the values stored in HPO section
+        hpos = document.getElementById("hpolist").value
+        var clean_hpos = clean_and_split_input(hpos);
+
         // get the values stored in the gene section
+        genes = document.getElementById("genelist").value;
+        var clean_genes = clean_and_split_input(genes);
+
         // search for co-occurances
-        for (var i = 0; i < clusters.length; i++) {
-            console.log(clusters[i]);
-            console.log('fucking fuck javascript')
-        }
+        search_clusters(clean_hpos, clean_genes, clusters)
+
     };
 };
